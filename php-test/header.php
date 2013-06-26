fd1d8a463992fa903e9bfb2c66102504c1d81ba3
Jessica Kung <jzkung@gmail.com>
2013-06-26 16:34:16 +0800

    Fixed the sticky header bug (cut off the start of the ad, added a secondary navbar, and converted everything to .htm in home2 to send to my boss. The positioning is strangely off for the input fields...

--- php-test/header.php
+++ php-test/header.php
@@ -1,13 +1,31 @@
 <div id="header">
 	<div id="logo"><a href="index.php">AnG</a></div>
 	<div id="login-register"><a href="#">登陆</a> | <a href="#">注册</a></div>
-	<div id="nav-bar">
-		<ul id="navlist">
-			<li id="first"><a href="#" class="navlist-active">首页</a></li>
-			<li><a href="#">产品和服务</a></li>
-			<li><a href="#">成功案例</a></li>
-			<li id="last"><a href="#">关于我们</a></li>
-		</ul>
+	<div id="navbars-wrapper">
+		<div id="main-navbar-wrapper">
+			<div id="main-navbar">
+				<ul>
+					<li id="main-navbar-home" class="first"><a href="#">首页</a></li>
+					<li id="main-navbar-products"><a class="navbar-active">产品和服务</a></li>
+					<li><a href="#">成功案例</a></li>
+					<li class="last"><a href="#">关于我们</a></li>
+				</ul>
+			</div>
+		</div>
+		<div id="secondary-navbar-wrapper">
+			<div id="secondary-navbar">
+				<ul id="secondary-home">
+					<li class="first"><a href="#" class="navbar-active">首页</a></li>
+					<li><a href="#">首页</a></li>
+					<li class="last"><a href="#">首页</a></li>
+				</ul>
+				<ul id="secondary-products">
+					<li class="first"><a href="#" class="navbar-active">产品和服务1</a></li>
+					<li><a href="#">产品和服务首页2</a></li>
+					<li class="last"><a href="#">产品和服务3</a></li>
+				</ul>
+			</div>
+		</div>
 	</div>
 </div>
 <div style="clear:both"></div>
--- php-test/index.php
+++ php-test/index.php
@@ -2,8 +2,17 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <script type="text/javascript" src="js/jquery-2.0.1.min.js"></script>
 <link rel="stylesheet" type="text/css" href="layout.css" />
+<link rel="stylesheet" type="text/css" href="home.css" />
 <link rel="stylesheet" type="text/css" media="only screen and (max-width: 768px), only screen and (max-device-width: 768px)" href="small.css" />
+<script src="js/waypoints.min.js"></script>
+<script src="js/waypoints-sticky.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#header').waypoint('sticky');
+    });
+</script>
 <title>AnG北京无双科技有限公司</title>
+</head>
 <?php
 $page_content = 'home.php';
 include('master.php');
