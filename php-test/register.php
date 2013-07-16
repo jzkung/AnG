9c7a94821bfa9dae7a35dc7b8c98c1abe11170e7
Kevin <klu1@stanford.edu>
2013-07-16 14:40:08 +0800

    removed random files, moved clearfix

--- php-test/register.php
+++ php-test/register.php
@@ -3,5 +3,7 @@ $page_content = 'register-content.php';
 $page_css = 'css/login-register.css';
 $small_css = 'css/small-login-register.css';
 $page_title = '注册一个AnG帐户';
+$main_navbar_active = '""';
+$secondary_navbar_active = '""';
 include('master.php');
 ?>
--- php-test/support.php
+++ php-test/support.php
@@ -4,5 +4,6 @@ $page_css = 'css/support.css';
 $small_css = 'css/small-support.css';
 $page_title = 'AnG北京无双科技有限公司';
 $main_navbar_active = '\'#main-navbar-support\'';
+$secondary_navbar_active = '""';
 include('master.php');
 ?>
