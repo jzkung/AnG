e74e8878d10dbec18f6951efa48f7d9559ba57e1
Jessica Kung <jzkung@gmail.com>
2013-07-16 18:02:37 +0800

    Made the 竞价助手 product page. Still need to add the right images.

---
+++ php-test/product-jjzs-content.php
@@ -0,0 +1,58 @@
+<div id="ad-jjzs">
+	<div id="ad-text-container">
+		<div id="ad-text-header" class="header1">AG竞价助手</div>
+		<div id="ad-text-description" class="description-text">智慧型竞价软件</div>
+		<button class="button blue-button" href="#">立即下载</button>
+	</div>
+</div>
+<div id="jjzs-panel-1" class="clearfix">
+	<img src="images/experience.png"/>
+	<div id="jjzs-panel-1-text-container">
+		<div id="jjzs-panel-1-text-header" class="header1">一键竞价</div>
+		<div id="jjzs-panel-1-text-description" class="description-text">
+			轻松完成海量复杂投放策略，对接主流搜索引擎，一键式提升效果降低成本CPC。
+		</div>
+		<button class="button blue-button">开始体验</button>
+	</div>
+</div>
+<div id="jjzs-features" class="features clearfix">
+	<div class="features-header">
+		<div class="header1">随心操作</div>
+		<div class="description-text">轻松完成多账户、跨帐户海量操作</div>
+	</div>
+	<div class="feature jjzs-feature">
+		<img src="images/features-1.png" class="center">
+		<div class="feature-text">
+			<div class="feature-text-header header3">AG广告管理平台</div>
+			<div class="feature-text-description">
+				智能竞价方案向导三套方案自由切换
+			</div>
+		</div>
+	</div>
+	<div class="feature jjzs-feature">
+		<img src="images/features-2.png" class="center">
+		<div class="feature-text">
+			<div class="feature-text-header header3">AG竞价助手</div>
+			<div class="feature-text-description">
+				竞价监控查看操作历史
+			</div>
+		</div>
+	</div>
+	<div class="feature jjzs-feature">
+		<img src="images/features-3.png" class="center">
+		<div class="feature-text">
+			<div class="feature-text-header header3">AG广告系统旗舰版</div>
+			<div class="feature-text-description">
+				高频、快速、无限量调整
+			</div>
+		</div>
+	</div>
+</div>
+<div id="cpc-panel">
+	<div class="header3">
+		<img src="images/products/jjzs-cpc-icon.jpg" class="center"><br/>
+		四种竞价策略灵活选择<br/>
+		独有CPC优化算法
+	</div>
+	<img id="cpc-panel-bg" src="images/products/jjzs-cpc.jpg">
+</div>
---
+++ php-test/product-jjzs.php
@@ -0,0 +1,9 @@
+<?php
+$page_content = 'product-jjzs-content.php';
+$page_css = 'css/products.css';
+$small_css = 'css/small-products.css';
+$page_title = 'AG竞价助手';
+$main_navbar_active = '\'#main-navbar-products\'';
+$secondary_navbar_active = '\'#secondary-products-2\'';
+include('master.php');
+?>
