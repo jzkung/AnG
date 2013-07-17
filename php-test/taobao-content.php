a1e00f966420d57766db97e79f0744bd305167c4
Kevin <klu1@stanford.edu>
2013-07-17 16:41:55 +0800

    added a jobs page?

---
+++ php-test/taobao-content.php
@@ -0,0 +1,13 @@
+<div id="top-ad">
+	<div id="ad-text-container">
+		<div id="ad-header">AG淘宝营销服务</div>
+		<div id="ad-description">asdfasdfasdfasdfasdfasdf</div>
+		<button id="ad-button">qwertyqwerty</button>
+	</div>
+</div>
+<div id="taobao-links clearfix">
+	<div id="links-text">asdf</div>
+	<div class="links-part">asdf:<a href="#">asdf</a></div>
+	<div class="links-part">asdf:<a href="#">asdf</a></div>
+	<div class="links-part">asdf:<a href="#">asdf</a></div>
+</div>
---
+++ php-test/taobao.php
@@ -0,0 +1,9 @@
+<?php
+$page_content = 'taobao-content.php';
+$page_css = 'css/taobao.css';
+$small_css = 'css/small-taobao.css';
+$page_title = 'AG淘宝营销服务';
+$main_navbar_active = '\'#main-navbar-products\'';
+$secondary_navbar_active = '\'#secondary-products-4\'';
+include('master.php');
+?>
