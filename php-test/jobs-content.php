e385cd837040497d944bbeeb168c72824d08e31e
Kevin <klu1@stanford.edu>
2013-07-17 16:02:30 +0800

    finished jobs.php

---
+++ php-test/jobs-content.php
@@ -0,0 +1,61 @@
+<h2 id="left-title">商务拓展经理</h2>
+<div class="clearfix">
+	<div id="left-content-wrapper">
+		<div class="left-list-wrapper">
+			<h4 class="list-title">工作职责：</h4>
+			<ul class="left-list">
+				<li>负责和相关合作伙伴的商务洽谈与拓展，建立深度合作；</li>
+				<li>根据公司发展情况，制定阶段性客户市场营销目标，通过营销活动等形式，拓展客户市场，协助完成公司业务整体布局；</li>
+				<li>根据公司战略规划，进行详细的市场调研，搜集市场动态和竞争对手信息，整理并编制客户资料；</li>
+				<li>了解和分析市场信息，根据市场变化及时调整、修订业务发展战略和计划，以适应市场的需要；</li>
+				<li>带领商务拓展团队开拓优质的客户，并进行大客户管理，包括客户的接洽、谈判和追踪以及客户的签约；</li>
+				<li>整合公司内部资源，拓展现有客户的实际需求；提升公司在客户方的影响力；</li>
+				<li>协助运营部门根据客户要求制定合理的运营计划，并在项目运作中协调客户与运营部门的关系，维系项目运作顺利完成，解决项；</li>
+				<li>目过程中产生的问题，做好客户的维护和异常问题的解决；</li>
+				<li>制定合理的团队业绩指标，高效地管理商务拓展团队，培养有潜力的团队成员；</li>
+				<li>建立、培训、管理公司业务团队，规范业务发展流程。</li>
+			</ul>
+		</div>
+		<div class="left-list-wrapper">
+			<h4 class="list-title">任职要求：</h4>
+			<ul class="left-list">
+				<li>3年以上销售或渠道推广工作经验，对互联网广告行业有独到认识及见解，具有淘宝网工作经验者优先。</li>
+				<li>具备优秀的市场开拓能力及经营决策能力，沟通良好，团队协作及组织能力优秀；</li>
+				<li>具备敏锐的市场洞察力和客户需求分析能力，能够根据市场情况制定切实有效的拓展策略；</li>
+				<li>具有敏锐的商业意识，善于把握客户需求，并具有极强商务谈判能力；</li>
+				<li>对工作认真负责并具有极强的责任心，具有高执行力和团队协作能力;</li>
+				<li>相貌端正、思维敏捷、性格活泼乐观、较强的亲和力;</li>
+				<li>出色的项目管理和分析解决问题能力，勇于承担工作压力。</li>
+			</ul>
+		</div>
+	</div>
+	<div id="right-content-wrapper">
+		<h3 class="right-title">您喜欢这个职位吗?</h3>
+		<form>
+			<div class="form-part">
+				<div class="form-description">姓名*</div>
+				<input type="text" id="name" name="name">
+			</div>
+			<div class="form-part">
+				<div class="form-description">手机*</div>
+				<input type="text" id="number" name="number">
+			</div>
+			<div class="form-part">
+				<div class="form-description">Email*</div>
+				<input type="text" id="email" name="email">
+			</div>
+			<div class="form-part">
+				<div class="form-description">Something*</div>
+				<input type="text" id="dummy" readonly>
+				<input type="file" id="file" name="file">
+				<div id="upload-button-container">
+					<button id="upload-button" onclick="return upload()">U</button>
+				</div>
+			</div>
+			<div id="submit-button-container">
+				<button id="submit-button" type="submit">Submit</button>
+			</div>
+		</form>
+	</div>
+</div>
+<script src="js/jobs.js"></script>
